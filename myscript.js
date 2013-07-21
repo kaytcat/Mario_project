2964b33048366d85749aec57e2493491e1d896af
Kaitlin Rathwell <kaytcat@gmail.com>
2013-07-22 01:23:46 +0200

    set boundries, added jump

--- myscript.js
+++ myscript.js
@@ -1,14 +1,19 @@
-var findPos = function(findClass) {
+var findPos = function(findClass, pos) {
   var startElement = $(findClass);
-  var startEleft = startElement.position().left;
-  var startLoc = startEleft  + "px";
+  var startEleft = startElement.position()[pos];
+  console.log(startElement.position());
+  var startLoc = startEleft;
   return startLoc;
 }
 
-$(document).ready(function() {
-	move1();
-	move2();
-});
+var findPostop = function(findClass) {
+  var startElement = $(findClass);
+  var startEleft = startElement.position().top;
+  var startLoctop = startEleft;
+  return startLoctop;
+}
+
+
 
 function move1() {
 		$('.clouds div:first-child').animate(
@@ -26,27 +31,55 @@ function move2() {
 		window.setTimeout(function() {move2()}, 500);
 };
 
-		
+	
+
+
 $(document).ready(function() {
+	move1();
+	move2();
     $(document).keydown(function(event) {
-        switch(event.which) {
-			case 65: case 37: //"A"
-				$('.character').animate({right: "+=10px"}, .01);
-				break;
-			case 83: case 40://"S"
-				$('.character').animate({bottom: "-=10px"}, .01);
-				break;
-			case 87: case 38 ://"W"
-				$('.character').animate({bottom: "+=10px"}, .01);
-				break;
-			case 68: case 39://"D"
-				$('.character').animate({right: "-=10px"}, .01);
-				break;
-			default:
-				break;
-		}
+
+	        switch(event.which) {
+				case 65: case 37: //"A"
+					if (findPos(".character", "left") > 0) {
+					$('.character').css({left: "-=10px"});
+					}
+					break;
+				case 83: case 40://"S"
+					console.log(findPos(".character","top"));
+					if (findPos(".character", "top") < 400) {
+					$('.character').css({top: "+=10px"});
+					}
+					break;
+				case 87: case 38 ://"W"
+					if (findPos(".character", "top") > 410) {
+					$('.character').css({top: "-=10px"});
+					}
+					break;
+				case 68: case 39://"D"
+					if (findPos(".character", "left") < 980) {
+					$('.character').css({left: "+=10px"});
+					}
+					break;
+				case 32: //jump (space)
+					$('.character').animate({top: "-=100px"}, 500);
+					$('.character').animate({top: "+=100px"}, 500);
+
+				default:
+					break;
+		}	
 	});
 });
 
 
-		
+function setPos(object, side, position) {
+	$(object).css({side: position});
+
+}
+
+
+
+
+
+
+
